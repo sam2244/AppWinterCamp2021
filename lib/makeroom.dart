242b24db4399052388fca5f974f6a4a719099156
sam2244 <49302466+sam2244@users.noreply.github.com>
2021-12-29 02:15:45 +0900

    makeroom page development
    add few form settings for making a new chat room

--- lib/makeroom.dart
+++ lib/makeroom.dart
@@ -1,7 +1,9 @@
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
 import 'login.dart';
 
 
@@ -11,6 +13,12 @@ class MakeroomPage extends StatefulWidget {
 
 class MakeroomPageState extends State<MakeroomPage > {
   int _selectedIndex = 0;
+  final _formKey = GlobalKey<FormState>(debugLabel: '_GuestBookState');
+  final _titlecontroller = TextEditingController();
+  final _maxnumcontroller = TextEditingController();
+  final int _maxnum = 2;
+  final int _maxsec = 60;
+  final int _category = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -60,10 +68,6 @@ class MakeroomPageState extends State<MakeroomPage > {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.camera),
-            label: '대기방',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.camera),
             label: '마이페이지',
           ),
         ],
@@ -71,9 +75,101 @@ class MakeroomPageState extends State<MakeroomPage > {
         onTap: _onItemTapped,
       ),
       body: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _titlecontroller,
+                        decoration: const InputDecoration(
+                          hintText: '방이름을 입력해주세요',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return '방이름을 입력해주세요';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(width: 20),
+                      TextFormField(
+                        controller: _maxnumcontroller,
+                        keyboardType: TextInputType.number,
+                        decoration: const InputDecoration(
+                          hintText: '방 최대인원을 입력해주세요 (2-4)',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty /*|| value < 2 || value > 4*/) {
+                            return '방 최대인원을 제대로 입력해주세요 (2-4)';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      await addRoom(_titlecontroller.text, _maxnum, _maxsec, await _Category(_category));
+                      _titlecontroller.clear();
+                    }
+                  },
+                  child: Row(
+                    children: const [
+                      Icon(Icons.add_rounded),
+                      SizedBox(width: 4),
+                      Text('Add'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+
+          ),
       ),
     );
   }
+
+  Future addRoom(String title, int max, int time, String category) async {
+    await FirebaseFirestore.instance.collection("rooms").add({
+      "title": title,
+      "maxnum": max,
+      'maxtime': time,
+      "category": category,
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+      'name': FirebaseAuth.instance.currentUser!.displayName,
+      'userId': FirebaseAuth.instance.currentUser!.uid,
+      'email': FirebaseAuth.instance.currentUser!.email,
+    });
+  }
+  Future<String> _Category(int index) async {
+    String categorytext ="";
+    if(index == 0){
+      categorytext = "상담";
+    }
+    else if(index == 1){
+      categorytext = "면접";
+    }
+    else if(index == 2){
+      categorytext = "토론";
+    }
+    else if(index == 3){
+      categorytext = "스터디";
+    }
+    else if(index == 4){
+      categorytext= "만남";
+    }
+    else if(index == 5){
+      categorytext = "수다";
+    }
+    return categorytext;
+  }
+
   void _onItemTapped(int index) {
     if(index == 0){
       Navigator.pushNamed(context, '/makeroom',
@@ -84,10 +180,6 @@ class MakeroomPageState extends State<MakeroomPage > {
       );
     }
     else if(index == 2){
-      Navigator.pushNamed(context, '/chat',
-      );
-    }
-    else if(index == 3){
       Navigator.pushNamed(context, '/mypage',
       );
     }
@@ -95,8 +187,8 @@ class MakeroomPageState extends State<MakeroomPage > {
       _selectedIndex = index;
     });
   }
+
   Future signOut() async {
-    // Trigger the authentication flow
     final FirebaseAuth auth = await FirebaseAuth.instance;
     await auth.signOut();
     uid = "";
